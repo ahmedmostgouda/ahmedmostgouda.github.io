0c530852d70a158c772792bf27e626d5acc316e6
unknown <ahmedmost.gouda@gmail.com>
2020-12-05 03:50:47 +0100

    Update the website

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-	$("#proj1card").load("proj1.html");
+	$("#brainDL_sem3_post").load("projectsposts/brainDL_sem3_post.html");
+	$("#atlas_sem3_post").load("projectsposts/atlas_sem3_post.html");
+	$("#histo_sem3_post").load("projectsposts/histo_sem3_post.html");
+	$("#dermo_sem3_post").load("projectsposts/dermo_sem3_post.html");
+	$("#chest_sem3_post").load("projectsposts/chest_sem3_post.html");
+	$("#brats_sem2_post").load("projectsposts/brats_sem2_post.html");
+
+	$("#mammo_sem2_post").load("projectsposts/mammo_sem2_post.html");
+	$("#fuzzy_sem2_post").load("projectsposts/fuzzy_sem2_post.html");
+	$("#ehealth_sem2_post").load("projectsposts/ehealth_sem2_post.html");
+	$("#heart_sem1_post").load("projectsposts/heart_sem1_post.html");
+	$("#harris_sem1_post").load("projectsposts/harris_sem1_post.html");
+	$("#cocacola_sem1_post").load("projectsposts/cocacola_sem1_post.html");
+
+	$("#GPStracking_self_post").load("projectsposts/GPStracking_self_post.html");
+	$("#osc_self_post").load("projectsposts/osc_self_post.html");
+
 	setBindings();
 });
 
